bd916d23800579dbd2c5c721a91eb80deafe2439
Chris <chedha@gmail.com>
2021-11-05 14:02:44 -0400

    Array sum files

---
+++ src/arraySumTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+class arraySumTest {
+
+	@Test
+	public void testArraySum () {
+		
+		int[] arr = new int[200000000];
+		Random rand = new Random();
+		
+		for (int i = 0; i < arr.length; i++) {
+	        arr[i] = rand.nextInt(10) + 1;
+	    }
+				
+		singleSum test = new singleSum();
+		
+		long start = System.currentTimeMillis();
+		int output = test.sum(arr);
+		System.out.println("Single sum time: " + (System.currentTimeMillis() - start + "ms")); // single time
+		
+		System.out.println();
+		
+		start = System.currentTimeMillis();
+		int pOutput = ParallelSum.parallelArraySum(arr);
+		System.out.println("Parallel sum time: " + (System.currentTimeMillis() - start) + "ms"); // parallel time
+		
+		assertEquals(pOutput, output);
+		
+	}
+
+}
